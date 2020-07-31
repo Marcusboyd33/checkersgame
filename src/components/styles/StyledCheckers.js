3c02470dea536a7303f47501c494c0ec99a0ccfd
Marcus Boyd <marcusboyd@Marcuss-MacBook-Pro.local>
2020-07-30 21:09:34 -0700

    add component structure

---
+++ src/components/styles/StyledCheckers.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledCheckersWrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: blue;
+    overflow: hidden;
+`
+
+export const StyledCheckers = styled.div`
+    display: flex;
+    align-items: flex-start;
+    padding: 40px;
+    margin: 0 auto;
+    max-width: 900px;
+`
